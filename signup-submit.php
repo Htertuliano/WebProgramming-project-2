59e8a5664a784a0591ff819bbad4765c52dee91b
Owen Bochner <52808582+owenbochner@users.noreply.github.com>
2021-03-23 18:21:48 -0400

    Add files via upload

--- signup-submit.php
+++ signup-submit.php
@@ -5,28 +5,13 @@ $user = $_POST["user"];
 $file = "users.txt";
 
 //txt data storage
-$output = $user . ":0,0,0,0,0,0" ."\n";
+$output = $user . ":" . array(0, 0, 0, 0, 0, 0)."\n";
 file_put_contents($file, $output, FILE_APPEND);
 ?>
 
-<div class="landing">
-    <center><img src="logo.png" alt="logo" height="75" width="75"></center>
+<h1> Welcome to Insert Game Name Here </h1>
+<p> So start the game sign in </p>
 
-    <h2> Welcome to the Mensa Club</h2>
-    <p> So start the game sign in below </p>
+<p> put a signin button here to go to the signin page </p>
 
-    <form action="signin-submit.php" method="post">    
-            <label><b>User Name:</b></label>
-            <br>
-            <br> 
-            <input required type="text" id="uname" name="user" size="32" maxlength="32" pattern="([a-zA-Z\s]){2,}">   
-            <br>
-            <br>
-            <a href="signup.html" id="signup">Not on Mensa Club yet? Sign up</a>
-            <br>
-            <br>
-            <br>
-            <input class="button" type="submit" value="Sign In">
-        </form>  
-</div>
 <?php include("footer.html"); ?>
