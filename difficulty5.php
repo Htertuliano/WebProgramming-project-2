5b346ba2fd13804bb50a56ade9d7d05372ade19a
Owen Bochner <52808582+owenbochner@users.noreply.github.com>
2021-03-27 13:53:44 -0400

    Add files via upload

---
+++ difficulty5.php
@@ -0,0 +1,115 @@
+<?php include("header.html"); ?>
+
+<h1> Difficulty 5 </h1>
+
+<div class="landing">
+     <form action="difficulty5-submit.php" method="post">
+        <fieldset>
+
+        <ol>
+            <!-- quetion 1 answer: reef CART(ONE)NJOYMEN(TWO)RDSMI(THREE)F -->
+            <li>
+                <h2>Which of the following words continues the sequence?</h2>
+                <b><h3> CARTON, ENJOYMENT, WORDSMITH <h3></h3>
+                <div>
+                    <input type="radio" name="q1" id="q1a" value="a">
+                    <label for="q1a"> copy </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1b" value="b">
+                    <label for="q1b"> reef </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1c" value="c">
+                    <label for="q1c"> cope </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1d" value="d">
+                    <label for="q1d"> rest </label>
+                </div>
+            </li>
+
+            <!-- question 2 answer: recluse-->
+            <li>
+                <h2> Which word means the same as ANCHORITE? </h2>
+                <div>
+                    <input type="radio" name="q2" id="q2a" value="a">
+                    <label for="q2a"> hieroglyphics </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2b" value="b">
+                    <label for="q2b"> trammel </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2c" value="c">
+                    <label for="q2c"> lackey </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2d" value="d">
+                    <label for="q2d"> recluse </label>
+                </div>
+            </li>
+
+            <!-- question 3 answer: clear soup-->
+            <li>
+                <h2> What is JULIENNE? </h2>
+
+                <div>
+                    <input type="radio" name="q3" id="q3a" value="a">
+                    <label for="q3a"> reed bunting </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3b" value="b">
+                    <label for="q3b"> an evergreen shrub </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3c" value="c">
+                    <label for="q3c">  a sleeveless jacket </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3d" value="d">
+                    <label for="q3d"> clear soup </label>
+                </div>
+            </li>
+
+
+            <!-- question 4 answer: stone-->
+            <li>
+                <h2> What is always associated with DOLMEN? </h2>
+                
+                <div>
+                    <input type="radio" name="q4" id="q4a" value="a">
+                    <label for="q4a"> clothes </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4b" value="b">
+                    <label for="q4b"> ironwork </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4c" value="c">
+                    <label for="q4c"> stone </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4d" value="d">
+                    <label for="q4d"> a statue </label>
+                </div>
+            </li>
+
+        </ol>
+
+        <input class="button" type="submit" value="Submit Difficulty 5">
+        </fieldset>
+     </form>
+</div>
