a5c3d03966ccc8a9242ff70e945d5e89eb107b40
Owen Bochner <52808582+owenbochner@users.noreply.github.com>
2021-03-27 17:07:03 -0400

    Add files via upload

---
+++ difficulty6.php
@@ -0,0 +1,119 @@
+difficulty6.php
+<?php include("header.html"); ?>
+
+<h1> Difficulty 6 </h1>
+
+<div class="landing">
+     <form action="difficulty6-submit.php" method="post">
+        <fieldset>
+
+        <ol>
+            <!-- quetion 1 -->
+            <li>
+                <h2>Which answer expresses the meaning of the specified word best?</h2>
+
+                <div>
+                    <input type="radio" name="q1" id="q1a" value="a">
+                    <label for="q1a"> compassionate </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1b" value="b">
+                    <label for="q1b"> comforting </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1c" value="c">
+                    <label for="q1c"> explanatory </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q1" id="q1d" value="d">
+                    <label for="q1d"> meddlesome </label>
+                </div>
+            </li>
+
+            <!-- question 2 -->
+            <li>
+                <h2> Which number logically follows this series? </h2>
+                <h3>  4     6     9     6     14     6      ...</h3>
+
+                <div>
+                    <input type="radio" name="q2" id="q2a" value="a">
+                    <label for="q2a"> 6 </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2b" value="b">
+                    <label for="q2b"> 17 </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2c" value="c">
+                    <label for="q2c"> 19 </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q2" id="q2d" value="d">
+                    <label for="q2d"> 21 </label>
+                </div>
+            </li>
+
+            <!-- question 3 -->
+            <li>
+                <h2> Water is to a pipe as ___ is to a wire</h2>
+
+                <div>
+                    <input type="radio" name="q3" id="q3a" value="a">
+                    <label for="q3a"> cord </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3b" value="b">
+                    <label for="q3b"> electricity </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3c" value="c">
+                    <label for="q3c"> heat </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q3" id="q3d" value="d">
+                    <label for="q3d"> gas </label>
+                </div>
+            </li>
+
+
+            <!-- question 4 -->
+            <li>
+                <h2> Which answer expresses the meaning opposite of that of the specified word best? </h2>
+                <h3> Tough </h3>
+
+                <div>
+                    <input type="radio" name="q4" id="q4a" value="a">
+                    <label for="q4a"> cowardly </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4b" value="b">
+                    <label for="q4b"> starch </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4c" value="c">
+                    <label for="q4c"> strong </label>
+                </div>
+
+                <div>
+                    <input type="radio" name="q4" id="q4d" value="d">
+                    <label for="q4d"> tender </label>
+                </div>
+            </li>
+
+        </ol>
+
+        <input class="button" type="submit" value="Submit Difficulty 6">
+        </fieldset>
+     </form>
+</div>
