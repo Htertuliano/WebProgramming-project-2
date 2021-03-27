dbb1b2472ae56c6b5ea021ac5dc06a68740b5b82
Owen Bochner <52808582+owenbochner@users.noreply.github.com>
2021-03-27 12:39:31 -0400

    Add files via upload

--- difficulty1.php
+++ difficulty1.php
@@ -7,104 +7,106 @@
         <fieldset>
 
         <ol>
-            <!-- quetion 1 -->
+            <!-- quetion 1 correct answer is explanatory -->
             <li>
-                <h2> Insert Question 1 here</h2>
-
+                <h2>Which answer expresses the meaning of the specified word best?</h2>
+                <b><h3> descriptive <h3></h3>
                 <div>
                     <input type="radio" name="q1" id="q1a" value="a">
-                    <label for="q1a"> an answer </label>
+                    <label for="q1a"> compassionate </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q1" id="q1b" value="b">
-                    <label for="q1b"> an answer </label>
+                    <label for="q1b"> comforting </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q1" id="q1c" value="c">
-                    <label for="q1c"> an answer </label>
+                    <label for="q1c"> explanatory </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q1" id="q1d" value="d">
-                    <label for="q1d"> an answer </label>
+                    <label for="q1d"> meddlesome </label>
                 </div>
             </li>
 
-            <!-- question 2 -->
+            <!-- question 2 answer: 19-->
             <li>
-                <h2> Insert Question 2 here</h2>
+                <h2> Which number logically follows this series? </h2>
+                <b><h3>  4     6     9     6     14     6      ...</h3></b>
 
                 <div>
                     <input type="radio" name="q2" id="q2a" value="a">
-                    <label for="q2a"> an answer </label>
+                    <label for="q2a"> 6 </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q2" id="q2b" value="b">
-                    <label for="q2b"> an answer </label>
+                    <label for="q2b"> 17 </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q2" id="q2c" value="c">
-                    <label for="q2c"> an answer </label>
+                    <label for="q2c"> 19 </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q2" id="q2d" value="d">
-                    <label for="q2d"> an answer </label>
+                    <label for="q2d"> 21 </label>
                 </div>
             </li>
 
-            <!-- question 3 -->
+            <!-- question 3 answer: electricity-->
             <li>
-                <h2> Insert Question 3 here</h2>
+                <h2> Water is to a pipe as ___ is to a wire</h2>
 
                 <div>
                     <input type="radio" name="q3" id="q3a" value="a">
-                    <label for="q3a"> an answer </label>
+                    <label for="q3a"> cord </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q3" id="q3b" value="b">
-                    <label for="q3b"> an answer </label>
+                    <label for="q3b"> electricity </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q3" id="q3c" value="c">
-                    <label for="q3c"> an answer </label>
+                    <label for="q3c"> heat </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q3" id="q3d" value="d">
-                    <label for="q3d"> an answer </label>
+                    <label for="q3d"> gas </label>
                 </div>
             </li>
 
 
-            <!-- question 4 -->
+            <!-- question 4 answer: tender-->
             <li>
-                <h2> Insert Question 3 here</h2>
+                <h2> Which answer expresses the meaning opposite of that of the specified word best? </h2>
+                <b><h3> Tough </h3></b>
 
                 <div>
                     <input type="radio" name="q4" id="q4a" value="a">
-                    <label for="q4a"> an answer </label>
+                    <label for="q4a"> cowardly </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q4" id="q4b" value="b">
-                    <label for="q4b"> an answer </label>
+                    <label for="q4b"> starch </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q4" id="q4c" value="c">
-                    <label for="q4c"> an answer </label>
+                    <label for="q4c"> strong </label>
                 </div>
 
                 <div>
                     <input type="radio" name="q4" id="q4d" value="d">
-                    <label for="q4d"> an answer </label>
+                    <label for="q4d"> tender </label>
                 </div>
             </li>
 
